274ef3536513d1decc1a798fe874a1d2809420f0
tornike2002 <toko.toka2000@gmail.com>
2023-09-14 22:19:44 +0400

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Components/Header/Head"
+import Banner from "./Components/Banner/Banner";
 
 function App() {
   return (
     <>
       <Header/>
+      <Banner />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,31 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      container: {
+				center: true,
+				screens: {
+					sm: '100%',
+					md: '100%',
+					lg: '1134px', 
+					xl: '1280px',
+				},
+      },
+      backgroundImage: {
+				'custom-bg': 'linear-gradient(180deg, #90658B 0%, #E4979E 100%)',
+			},
+      fontFamily: {
+				firago: ['FiraGO', 'sans-serif'],
+			},
+      backgroundColor: {
+				'banner-bg-blue': '#178ADB',
+				'banner-bg-yellow': '#D0F0C0',
+			},
       colors: {
         addButtonColor: "#D0F0C0",
         logInButtonColor: "#EAEAEA",
         badgeColor: "#CD2727",
         badgeText: "#F8F8F8",
+        
       },
     },
   },
