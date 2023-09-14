cb2ddbf625f834f452dfa138d75cb672f9c08c42
tornike2002 <toko.toka2000@gmail.com>
2023-09-14 21:52:48 +0400

    added banner component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '1134px', // Set the width to 1134px for large screens
+          xl: '1280px',
+        },
+      },
+			backgroundImage: {
+				'custom-bg': 'linear-gradient(180deg, #90658B 0%, #E4979E 100%)',
+			},
+			fontFamily: {
+				firago: ['FiraGO', 'sans-serif'],
+			},
+			colors: {
+				white: '#fff',
+			},
+			backgroundColor: {
+				'banner-bg-blue': '#178ADB',
+				'banner-bg-yellow': '#D0F0C0',
+			},
+		},
+	},
+	plugins: [],
+};
