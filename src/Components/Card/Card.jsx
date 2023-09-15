8fcce883472d8a53b9e1784e14293164e458fed1
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-09-15 13:15:31 +0400

    პროდუქტის სახელის სიგრძის ლიმიტირება 27 სიმბოლოზე

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -10,22 +10,28 @@ const TestProduct = {
 }
 
 export default function Card({ product }) {
-    product = TestProduct;
+
+    if(product == undefined) {  // მხოლოდ საცდელად
+        product = TestProduct;
+    }
+
     return (
-        <div className=" flex flex-col items-center gap-1 w-48 m-12 p-2 rounded-2xl bg-white leading-4">
+        <div
+            className=" flex flex-col items-center gap-1 w-48 m-12 p-2 rounded-2xl bg-white leading-4">
             <img
                 className=" w-40 h-40 p-2 object-center"
-                src={product?.image || noImage} alt=""
+                src={product?.image || noImage}
+                alt={product?.name + " picture"}
             />
 
             <div className=" flex justify-between w-32">
                 <h3 className=" font-medium">{(product?.price - product?.price * product?.discount / 100) || "---"} ₾</h3>
+                
                 { product?.discount > 0 && <h3 className=" font-medium line-through text-red-500">{product?.price || "---"} ₾</h3>}
             </div>
 
             <div className="py-2 w-32">
-                <h3 className="relative font-medium h-8.5 overflow-hidden
-                                after:content-['...'] after:bg-white after:absolute after:right-0 after:bottom-0 after:pe-2">{ product.name }</h3>
+                <h3 className="relative font-medium h-8.5 overflow-hidden">{ product.name.length > 27 ? product.name.slice(0, 26) + "..." : product.name }</h3>
             </div>
 
             <div className=" flex justify-between gap-20">
